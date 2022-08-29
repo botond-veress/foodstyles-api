a7a8437687bcf5285bb02686d3712eec7c68c131
Botond Veress <botond.veress@gmail.com>
2022-08-29 03:35:48 +0300

    fix: throw custom error when the access token is invalid or expired

--- src/auth/guard/graphql-jwt.guard.ts
+++ src/auth/guard/graphql-jwt.guard.ts
@@ -1,6 +1,7 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 @Injectable()
 export class GraphqlJwtGuard extends AuthGuard('jwt') {
@@ -8,5 +9,13 @@ export class GraphqlJwtGuard extends AuthGuard('jwt') {
     const ctx = GqlExecutionContext.create(context);
 
     return ctx.getContext().req;
+  }
+
+  handleRequest(err: any, user: any, info: any, context: any, status: any) {
+    if (info instanceof JsonWebTokenError) {
+      throw new UnauthorizedException('The token is invalid.');
+    }
+
+    return super.handleRequest(err, user, info, context, status);
   }
 }
